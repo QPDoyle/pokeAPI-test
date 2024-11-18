82312040a6841747656cd9345f82bcfa425e2cbf
QPDoyle <qpdoyle13@gmail.com>
2024-11-18 12:35:53 -0500

    front-end to backend-linking

--- server/app.js
+++ server/app.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 const app = express();
 
-app.use('/website', express.static(path.join(__dirname, '/public')));
+app.get('/api', (req, res)=>{
+    res.json(`HTTP GET Request Recieved.`);
+}) 
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+//app.use('/website', express.static(path.join(__dirname, '/public')));
 
 app.use((req,res) => {
     res.status('404');
-    res.send(`<h1>Error 404: Recourse not found</h1>`);
+    res.send(`<h1>Error 404: Resourse not found</h1>`);
 
 })
 
 app.listen(3000, () =>{
-    console.log("beaver");
+    console.log("Website is live!");
 });
